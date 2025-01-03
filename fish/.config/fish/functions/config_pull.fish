05ac31e836f615598f218061bf80d37db978bf3c
Bob Florian <bobflorian@planstone.com>
2025-01-03 10:12:39 -0600

    Update Fish config: 2025-01-03

--- fish/.config/fish/functions/config_pull.fish
+++ fish/.config/fish/functions/config_pull.fish
@@ -1,5 +1,44 @@
 function config_pull --description 'Pull latest Fish config'
     cd ~/dotfiles
     git pull
+    
+    # Clear Mac-specific Z plugin paths
+    set --erase Z_DATA
+    set --erase Z_DATA_DIR 
+    set --erase Z_EXCLUDE
+
+    # Create directories
+    mkdir -p ~/.config/fish/{functions,conf.d,completions}
+    mkdir -p ~/.local/share/z
+
+    # Check and install Fisher (plugin manager)
+    if not functions -q fisher
+        echo "Installing Fisher plugin manager..."
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+    end
+
+    # Common plugins to check/install
+    set plugins \
+        "jorgebucaran/fisher" \
+        "jethrokuan/z" \
+        "PatrickF1/fzf.fish" \
+        "edc/bass"
+
+    for plugin in $plugins
+        if not fisher list | grep -q $plugin
+            echo "Installing $plugin..."
+            fisher install $plugin
+        end
+    end
+
+    # Copy config files
+    cp -r fish/.config/fish/functions/* ~/.config/fish/functions/
+    cp -r fish/.config/fish/conf.d/* ~/.config/fish/conf.d/
+    cp fish/.config/fish/config.fish ~/.config/fish/
+    
+    if test -f fish/.config/fish/env.fish
+        cp fish/.config/fish/env.fish ~/.config/fish/
+    end
+    
     cd -
 end
